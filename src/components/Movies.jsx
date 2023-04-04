b427daa4267b1b036d9ec0f05de1af18429ea018
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-04 16:05:19 +0300

    css design for Movie.jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,17 +1,29 @@
 "use client";
 import React from "react";
+
+//Next Docs
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Movies = ({ dt }) => {
+  const router = useRouter();
+
   console.log(dt);
   return (
-    <div className="min-w-[400px] h-[300px]">
+    <div className="min-w-[470px] relative  imgContainer cursor-pointer">
       <Image
-        fill
+        width={470}
+        height={300}
         src={`https://image.tmdb.org/t/p/original/${
           dt?.backdrop_path || dt?.poster_path
         }`}
       />
+      <div className="absolute bottom-0 p-3 w-full h-full flex flex-col justify-end opacity-0 hover:opacity-100 transition-opacity duration-700">
+        <div className="text-2xl font-bold">{dt.title}</div>
+        <div className="">
+          {dt.release_date} - {dt.vote_average}
+        </div>
+      </div>
     </div>
   );
 };
