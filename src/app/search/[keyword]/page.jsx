624c0a95e3ee5f05b16ac74c6d8bb3df21967ea0
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-04 17:55:02 +0300

    search movie finished

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,7 +1,29 @@
+import Movies from "@/components/Movies";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const page = async ({ params }) => {
+  const keyword = params.keyword;
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=840a19c8a5311f5ddb65f56b74f1dc37&query=${keyword}&language=en-US&include_adult=false`
+  );
+  const data = await res.json();
+
+  console.log(data, "data");
+
+  return (
+    <div>
+      {!data?.results ? (
+        <div className="">Aranılan Film Bulunamadı.</div>
+      ) : (
+        <div className="flex items-center justify-center flex-wrap gap-3">
+          {data?.results?.map((dt, i) => (
+            <Movies key={i} dt={dt} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default page;
