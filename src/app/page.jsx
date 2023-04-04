c8a523b4c9cd619dd08b5cd99bc166736d244eff
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-04 15:24:44 +0300

    finished tab.jsx and data fetching from the server(page.jsx)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
-const page = () => {
+const page = async ({ searchParams }) => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      searchParams.genre ? "movie/" + searchParams.genre : "trending/all/day"
+    }?api_key=840a19c8a5311f5ddb65f56b74f1dc37&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  const data = await res.json();
+
+  console.log(data);
+
   return (
     <div>
       {" "}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">veri çekme alanı</h1>
     </div>
   );
 };
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,37 @@
+"use client";
+//Link
+import Link from "next/link";
+//React
 import React from "react";
+//Search Params
+import { useSearchParams } from "next/navigation";
 
 const Tabs = () => {
-  return <div>Tabs</div>;
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+
+  const tabs = [
+    { name: "En Populer", url: "popular" },
+    { name: "En Son", url: "latest" },
+    { name: "Yakında Gelecekler", url: "upcoming" },
+  ];
+
+  return (
+    <div className="p-5 my-3 bg-gray-100 dark:bg-gray-900 flex items-center justify-center gap-7">
+      {tabs.map((tab, i) => (
+        <Link
+          className={`cursor-pointer hover:opacity-50 transition-opacity-ease-out duration-100 ${
+            tab.url === genre
+              ? "underline underline-offset-8 text-amber-500"
+              : ""
+          } `}
+          href={`/?genre=${tab.url}`}
+        >
+          {tab.name}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Tabs;
